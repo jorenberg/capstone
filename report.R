9c91852a80116746d7d209c7b1c562bf4b60be05
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-18 21:38:46 +0530

    Initial commit
    Capstone®, by Prabhat Kumar, http://prabhatkumar.org/.

--- report.R
+++ report.R
@@ -1,1 +1,11 @@
 #!/usr/bin/env r
+
+
+# Capstone®
+# ------------------------------------------
+# Exploratory Data Analysis and Modelling.
+# ------------------------------------------
+# by Prabhat Kumar, http://prabhatkumar.org/
+# Date    :   18-March-2016
+# ==========================================
+# locale  :   en_US.UTF-8
