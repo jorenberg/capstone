5747b41d75aea5703fe27e16b74babc62953901e
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-19 17:54:31 +0530

    Initial commit
    Cleaning the SwiftKey Data.

--- report.R
+++ report.R
@@ -30,3 +30,37 @@ unzip("./data/SwiftKey.zip")
 enBlogs   <- readLines("./data/final/en_US/en_US.blogs.txt",   encoding = "UTF-8", skipNul = TRUE)
 enNews    <- readLines("./data/final/en_US/en_US.news.txt",    encoding = "UTF-8", skipNul = TRUE)
 enTwitter <- readLines("./data/final/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul = TRUE)
+
+
+## [03] Cleaning the SwiftKey Data.
+## ================================
+## working with stringi:
+## http://www.rexamine.com/resources/stringi/
+## Author: Marek Gagolewski, gagolews@rexamine.com/
+library(stringi)
+
+# some removal of non english characters to need to be done to make the data more usable.
+# drop non UTF-8 characters.
+blogs   <- iconv(enBlogs,   from = "latin1", to = "UTF-8", sub = "")
+news    <- iconv(enNews,    from = "latin1", to = "UTF-8", sub = "")
+twitter <- iconv(enTwitter, from = "latin1", to = "UTF-8", sub = "")
+
+# Replace Occurrences of a Pattern.
+twitter <- stri_replace_all_regex(twitter, "\u2019|`","'")
+twitter <- stri_replace_all_regex(twitter, "\u201c|\u201d|u201f|``",'"')
+
+# Save a single object to file.
+saveRDS(blogs,   "./enBlogs.rds")
+saveRDS(news,    "./enNews.rds")
+saveRDS(twitter, "./enTwitter.rds")
+
+# A data frame is a list of variables of the same number of rows with unique row names,
+# given class "data.frame". If no variables are included,
+# the row names determine the number of rows.
+data.frame(blogs = length(blogs), news = length(news), twitter = length(twitter))
+
+# ---------------------------
+#   blogs   news      twitter
+# 1 899288  1010242   2360148
+# class     :       character
+# ---------------------------
