ba48a2089c2731b820a462736036c016a2f6fdfb
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-20 17:58:28 +0530

    Initial commit
    Capstone®, by Prabhat Kumar, http://prabhatkumar.org/.

--- plot.R
+++ plot.R
@@ -1,1 +1,15 @@
 #!/usr/bin/env r
+
+
+# Capstone®
+# ------------------------------------------
+# To Plotting of the SwiftKey Data.
+# ------------------------------------------
+# by Prabhat Kumar, http://prabhatkumar.org/
+# Date    :   20-March-2016
+# ==========================================
+# locale  :   en_US.UTF-8
+# ==========================================
+# Data Source   : SwiftKey, Inc.
+# HomePage      : https://swiftkey.com/en/
+# ==========================================
