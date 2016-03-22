5422b5843b23a7357216fd3ea62bf83cf88534de
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-22 18:31:59 +0530

    Initial commit
    Capstone® - [Predictive Modelling], by Prabhat Kumar, http://prabhatkumar.org/.

--- prediction/model/frequencyTable.R
+++ prediction/model/frequencyTable.R
@@ -1,1 +1,15 @@
 #!/usr/bin/env r
+
+
+# Capstone®
+# ------------------------------------------
+# Predictive Modelling.
+# ------------------------------------------
+# by Prabhat Kumar, http://prabhatkumar.org/
+# Date          : 22-March-2016
+# ==========================================
+# locale        : en_US.UTF-8
+# ==========================================
+# Data Source   : SwiftKey, Inc.
+# HomePage      : https://swiftkey.com/en/
+# ==========================================
