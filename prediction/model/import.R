ceb9a46c4370d064d2442a2bf3c059eaded38d1f
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-23 13:00:19 +0530

    Initial commit
    Cleaning the SwiftKey Data.

--- prediction/model/import.R
+++ prediction/model/import.R
@@ -22,3 +22,19 @@ rm(list = ls())
 enBlogs   <- readLines("./data/final/en_US/en_US.blogs.txt",   encoding = "UTF-8", skipNul = TRUE)
 enNews    <- readLines("./data/final/en_US/en_US.news.txt",    encoding = "UTF-8", skipNul = TRUE)
 enTwitter <- readLines("./data/final/en_US/en_US.twitter.txt", encoding = "UTF-8", skipNul = TRUE)
+
+# ¬ Cleaning the SwiftKey Data.
+# working with stringi:
+# http://www.rexamine.com/resources/stringi/
+# Author: Marek Gagolewski, gagolews@rexamine.com/
+library(stringi)
+
+# some removal of non english characters to need to be done to make the data more usable.
+# drop non UTF-8 characters.
+blogs   <- iconv(enBlogs,   from = "latin1", to = "UTF-8", sub = "")
+news    <- iconv(enNews,    from = "latin1", to = "UTF-8", sub = "")
+twitter <- iconv(enTwitter, from = "latin1", to = "UTF-8", sub = "")
+
+# Replace Occurrences of a Pattern.
+twitter <- stri_replace_all_regex(twitter, "\u2019|`","'")
+twitter <- stri_replace_all_regex(twitter, "\u201c|\u201d|u201f|``",'"')
