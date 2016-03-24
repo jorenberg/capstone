b2e2d9cf00ef3d03c418e4ccb7c20fa304d3cbeb
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-24 21:52:42 +0530

    Initial commit
    Sampling the SwiftKey Data.

--- prediction/model/sample.R
+++ prediction/model/sample.R
@@ -17,3 +17,18 @@
 
 # Ω - Specified Environment Access.
 envir = .GlobalEnv
+
+if (is.environment(envir)) {
+  print("Environment is Global!")
+  # ¬ Sampling the SwiftKey Data.
+  # Setting the seed for reproducibility.
+  set.seed(100)
+  
+  # This function takes a random sample of the data.
+  sampler <- function(chunk, percent) {
+    percent <- round(length(chunk)*percent)
+    sample.index <- sample(1:length(chunk), percent)
+    
+    return(chunk[sample.index])
+  }
+}
