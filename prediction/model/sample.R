d441c706ae12ae00b0a4eccc9b87449db76da95d
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 00:55:09 +0530

    Initial commit
    Let's start off with a 20% sample data, by using Anonymous function.

--- prediction/model/sample.R
+++ prediction/model/sample.R
@@ -32,3 +32,40 @@ if (is.environment(envir)) {
     return(chunk[sample.index])
   }
 }
+
+# ¬ Let's start off with a 20% sample data.
+#                         -----
+(function(){
+  if (!"./data/sample" %in% dir("./data/")) {
+    print("Creating a -Sample- Directory, please wait...")
+    dir.create("data/sample", showWarnings = TRUE)
+  }
+  
+  print("Making 20% of sample data...")
+  
+  # //1-US.blogs//
+  sampleBlogs <- sampler(blogs, .20)
+  # Write Lines to a Connection.
+  writeLines(c(sampleBlogs), "./data/sample/sampleBlogs.txt")
+  
+  # //2-US.news//
+  sampleNews <- sampler(news, .20)
+  # Write Lines to a Connection.
+  writeLines(c(sampleNews), "./data/sample/sampleNews.txt")
+  
+  # //3-US.twitter//
+  sampleTwitter <- sampler(twitter, .20)
+  # Write Lines to a Connection.
+  writeLines(c(sampleTwitter), "./data/sample/sampleTwitter.txt")
+  
+  # List the Files in a Directory/Folder.
+  list.dirs(path = "./data", full.names = TRUE, recursive = TRUE)
+  
+  # Information about Files.
+  file.info("./data/sample/sampleBlogs.txt")
+  file.info("./data/sample/sampleNews.txt")
+  file.info("./data/sample/sampleTwitter.txt")
+  
+  print("Files are written, in text format.")
+
+})()
