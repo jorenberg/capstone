32adc75d0b95b8e3556e49b1c7d1bf3e50fe19aa
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 12:18:21 +0530

    Initial commit
    Usage: Rcpp::sourceCpp('..path/init.cpp').

--- prediction/model/init.cpp
+++ prediction/model/init.cpp
@@ -13,3 +13,16 @@ using namespace Rcpp;
 // Data Source   : SwiftKey, Inc.
 // HomePage      : https://swiftkey.com/en/
 // ==========================================
+
+
+// This is a C++ Incorporation for exporting a C++ functions to R.
+//           ~~~~~~~~~~~~~~~~~
+// You can source this function into an R session using the - 
+// Rcpp::sourceCpp function (or via the Source button on the editor toolbar).
+// Learn more about Rcpp at:
+//
+//    --------------------------------
+//    http://www.rcpp.org/
+//    http://gallery.rcpp.org/
+//    http://adv-r.had.co.nz/Rcpp.html
+//    --------------------------------
