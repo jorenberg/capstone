4afcfbf52ca24b7ff76086f8d33bff63b89fa842
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 12:14:09 +0530

    Initial commit
    Capstone® - [Predictive Modelling], by Prabhat Kumar, http://prabhatkumar.org/.

--- prediction/model/init.cpp
+++ prediction/model/init.cpp
@@ -1,2 +1,15 @@
 #include <Rcpp.h>
 using namespace Rcpp;
+
+// Capstone®
+// ------------------------------------------
+// Predictive Modelling.
+// ------------------------------------------
+// by Prabhat Kumar, http://prabhatkumar.org/
+// Date          : 24-March-2016
+// ==========================================
+// locale        : en_US.UTF-8
+// ==========================================
+// Data Source   : SwiftKey, Inc.
+// HomePage      : https://swiftkey.com/en/
+// ==========================================
