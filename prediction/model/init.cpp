ba1dea9c6097a4c63d522064088ec85c48818eaf
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 12:25:17 +0530

    Initial commit
    Help: Include R code blocks.

--- prediction/model/init.cpp
+++ prediction/model/init.cpp
@@ -26,3 +26,8 @@ using namespace Rcpp;
 //    http://gallery.rcpp.org/
 //    http://adv-r.had.co.nz/Rcpp.html
 //    --------------------------------
+
+
+// You can include R code blocks in C++ files processed with sourceCpp
+// (useful for testing and development).
+// The R code will be automatically, run after the compilation.
