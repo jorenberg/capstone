5ef2ab45ff5b05a51492701d05886cccc575042e
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 21:59:39 +0530

    Initial commit
    kmeans.h, k-means clustering for cluster analysis.

---
+++ prediction/model/src/kmeans.h
@@ -0,0 +1,12 @@
+// Capstone®
+// ------------------------------------------
+// Predictive Modelling.
+// ------------------------------------------
+// by Prabhat Kumar, http://prabhatkumar.org/
+// Date          : 24-March-2016
+// ==========================================
+// locale        : en_US.UTF-8
+// ==========================================
+// Data Source   : SwiftKey, Inc.
+// HomePage      : https://swiftkey.com/en/
+// ==========================================
