afbdc98544c29d424394c22e76d931587b191968
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-25 23:34:02 +0530

    Initial commit
    Definition of k-means clustering.

--- prediction/model/src/kmeans.h
+++ prediction/model/src/kmeans.h
@@ -30,3 +30,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 // ========================================================================
+
+
+// k-means clustering is a heuristic method of vector quantization,
+// that is popular for cluster analysis in data mining.
+// k-means clustering aims to partition "n" observations into "k" clusters
+// in which each observation belongs to the cluster with the nearest mean,
+// serving as a prototype of the cluster.
+// This results in a partitioning of the data space into Voronoi cells.
