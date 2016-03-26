92f634269ce0844645022d47bea86854a41e570b
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-26 17:10:21 +0530

    Initial commit
    The problem is computationally difficult (NP-hard), in Computational Complexity Theory.

--- prediction/model/src/kmeans.h
+++ prediction/model/src/kmeans.h
@@ -38,3 +38,5 @@
 // in which each observation belongs to the cluster with the nearest mean,
 // serving as a prototype of the cluster.
 // This results in a partitioning of the data space into Voronoi cells.
+
+// Computational Complexity: NP-hard
