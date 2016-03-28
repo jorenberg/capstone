74a472174b724ed7069e8774157dc7d5464a5320
Prabhat Kumar <prabhat.genome@gmail.com>
2016-03-28 12:36:07 +0530

    Initial commit
    Capstone® - [Shiny App], by Prabhat Kumar, http://prabhatkumar.org/.

--- App/helpers.R
+++ App/helpers.R
@@ -1,1 +1,15 @@
 #!/usr/bin/env Rscript
+
+
+# Capstone®
+# ------------------------------------------
+# Predictive Shiny App.
+# ------------------------------------------
+# by Prabhat Kumar, http://prabhatkumar.org/
+# Date          : 28-March-2016
+# ==========================================
+# locale        : en_US.UTF-8
+# ==========================================
+# Data Source   : SwiftKey, Inc.
+# HomePage      : https://swiftkey.com/en/
+# ==========================================
